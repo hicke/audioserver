0356de206a1997fd09bec9d3b13f7ffb4e8eaa4f
Henric Helbro <henric.helbro@gmail.com>
2015-10-15 23:46:06 +0200

    fixat sr.py

--- sr_downloader.py
+++ sr_downloader.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python
 
-import urllib2
 import urllib
 import json
 import os
 
+# Get json from sr.se
 response = urllib.urlopen('http://api.sr.se/api/v2/episodes/index?programid=2000&format=json&audioquality=hi&page=1&size=1')
 data = json.load(response)
-file_url = data['episodes'][0]['downloadpodfile']['url']
-file = urllib2.urlopen(file_url)
-file_name = file_url.split('/')[-1]
 
-os.system('curl -O %s' %file_url)
+# Extract the correct url from json
+url = data['episodes'][0]['downloadpodfile']['url']
+
+# Extract correct filename
+mp3 = url.split('/')[-1]
+
+# Open correct url
+f = urllib.urlopen(url)
+
+# Check if file exists
+if os.path.exists(mp3):
+    print("File exists")
+else:
+    # Open file and set as binary
+    fh = open(mp3, 'wb')
+    # Write binary file to disk
+    fh.write(f.read())
+    # Close file
+    fh.close()
