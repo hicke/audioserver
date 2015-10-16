d74249d3d474102890197251c90aa53232513aa9
Henric Helbro <henric.helbro@gmail.com>
2015-10-16 14:46:46 +0200

    New commit

--- info.py
+++ info.py
@@ -3,14 +3,15 @@
 # Todo
 #
 # Check if Land- or Sjöväder
-# If there is a new mp3-file, delete old
-# POST to business layer that new file has arrived
+# If there is a titleLower mp3-file, delete old
+# POST to business layer that titleLower file has arrived
 # Download image from SR
 
 
 import urllib
 import json
 import os
+import time
 
 response = urllib.urlopen('http://api.sr.se/api/v2/episodes/index?programid=2000&format=json&audioquality=hi&page=1&size=1')
 data = json.load(response)
@@ -20,11 +21,15 @@ title = data['episodes'][0]['downloadpodfile']['title']
 imageUrl = data['episodes'][0]['imageurltemplate']
 length = data['episodes'][0]['downloadpodfile']['duration']
 seaweather = "sjöväder"
-new = title.encode("utf8").lower()
+titleLower = title.encode("utf8").lower()
+publishedTimeData = data['episodes'][0]['downloadpodfile']['publishdateutc']
 
+# Converts utc to unix epoch time
+publishedTime = publishedTimeData[6:16]
+# currentTime = int(time.time())
 
-#Check if title contains Sjöväder
-if seaweather in new:
+# Check if title contains Sjöväder
+if seaweather in titleLower:
     print title
 else:
     print "Fail"
