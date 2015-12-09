8bc582885eef64025b31bb44d06667e5d1abfedb
Henric Helbro <henric.helbro@gmail.com>
2015-12-09 21:59:03 +0100

    merge

--- master.py
+++ master.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
 
-def main():
-    text_file = open("Output.txt", "w")
-    for i in range(1, 32):
-        print i
-    text_file.close()
 
-# Standard boilerplate to call the main() function to begin
-# the program.
-if __name__ == '__main__':
-  main()
-# text_file.close()
+class DumpList(object):
+    """docstring for DumpList"""
+    def __init__(self, stop):
+        self.stop = stop
+    def Looper(self):
+        return self.stop
+
+DumpList(2).Looper()
+# class FileLooper()
+#     def main(self):
+#         text_file = open("Output.txt", "w")
+#         for i in range(1, 32):
+#             print "Dag: " + str(i) + '\n'
+#     text_file.close()
